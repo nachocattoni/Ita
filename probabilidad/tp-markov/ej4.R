3a4d6180517bbe42675a347928bc04c27ac46951
Maxi Redigonda <maxiredigonda@gmail.com>
2017-11-30 14:12:04 -0300

    Agrega ejercicio 4

--- probabilidad/tp-markov/ej4.R
+++ probabilidad/tp-markov/ej4.R
@@ -11,6 +11,9 @@ validarFila <- function(v) {
   return(TRUE)
 }
 
+# Realiza la simulación con los parámetros especificados,
+# y retorna la cantidad de veces que visitó cada estado como
+# una tabla.
 simulacionMarkov <- function(tMatrix, initial, rep) {
   # Check initial (ejercicio a)
   if(!validarFila(initial)) return(FALSE)
@@ -23,22 +26,30 @@ simulacionMarkov <- function(tMatrix, initial, rep) {
   }
 
   # Hacer la simulación (ejercicio c)
-  MC <- new("markovchain", states=c("A", "B", "C", "D"), transitionMatrix=tMatrix)
-  init <- sample(c("A", "B", "C", "D"), 1, prob=initial)
+  estados <- c("A", "B", "C", "D")
+  MC <- new("markovchain", states=estados, transitionMatrix=tMatrix)
+  init <- sample(estados, 1, prob=initial)
 
   if(length(absorbingStates(MC)) > 0) return(FALSE) # No debe tener estados absorbentes
   lista <- rmarkovchain(n=rep, object=MC, t0=init)
 
-  # Graficarla! (enunciado)
-  print(lista)
-  # plot(lista)
-  return(TRUE)
+  listaPlot <- replicate(length(lista), 0)
+  for (i in 1:length(lista)) {
+    listaPlot[i] <- match(lista[i], estados)
+  }
+
+  # Gráfico (enunciado)
+  pdf("markovchain.pdf")
+  plot(listaPlot, yaxt="n", type='b')
+  axis(2, at=1:4, labels=c("A", "B", "C", "D"))
+  dev.off();
+
+  return(table(lista))
 }
-v <- (matrix(c(0, .25, .5, .25, .75, 0, .12, .13, .1, .1, 0, .8, .2, .2, .6, 0), byrow=TRUE, nrow=4))
-print(v)
+
 simulacionMarkov(matrix(c( 0 , .25, .5 , .25,
                           .75,  0 , .12, .13,
                           .1 , .1 ,  0 , .8, 
-                          .2 , .2 , .6 ,  0), byrow=TRUE, nrow=4), c(.25, .25, .25, .25), 100)
+                          .2 , .2 , .6 ,  0), byrow=TRUE, nrow=4), c(1/8, 2/8, 1/8, 4/8), 20)
 
 
