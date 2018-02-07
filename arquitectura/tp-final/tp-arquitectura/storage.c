686f30ee4561dc5ccc677c7e0266cf710a4075ce
Maxi Redigonda <maxiredigonda@gmail.com>
2018-02-07 10:06:28 -0300

    Makes bucket work like a set

--- arquitectura/tp-final/tp-arquitectura/storage.c
+++ arquitectura/tp-final/tp-arquitectura/storage.c
@@ -14,7 +14,13 @@ Bucket create_new_bucket(){
     return B;
 }
 
-void insert_element(Bucket *B, const char *v){
+int insert_element(Bucket *B, const char *v){
+    int i;
+    for(i = 0; i < B->nelems; i++){
+        if(strcmp(B->storage[i], v) == 0){
+            return i;
+        }
+    }
     if(B->nelems >= B->sz){
         B->storage = (char **) realloc(B->storage, (B->sz * 2) * sizeof(char *));
         B->sz *= 2;
@@ -22,4 +28,5 @@ void insert_element(Bucket *B, const char *v){
     B->storage[B->nelems] = (char *) malloc(MAX_VARIABLE_NAME_SIZE * sizeof(char));
     strcpy(B->storage[B->nelems], v);
     B->nelems++;
+    return B->nelems - 1;
 }
--- arquitectura/tp-final/tp-arquitectura/storage.h
+++ arquitectura/tp-final/tp-arquitectura/storage.h
@@ -22,7 +22,10 @@ Bucket create_new_bucket();
  * @param B El bucket al cual se le quiere agregar el elemento.
  * @param v La cadena de texto que se quiere meter en el _Bucket.
  * Introduce el elemento en el bucket, si es que no existe todavía.
+ * @return Si el elemento ya existe en el bucket, retorna la posición
+ * en la que se encuentra. De otra manera, lo inserta al final, y 
+ * retorna esta posición.
  */
-void insert_element(Bucket *B, const char *v);
+int insert_element(Bucket *B, const char *v);
 
 #endif // STORAGE_H
